0b5e7cd57cb35bbe812c81a64bb76bd549393307
heinriciro <ianhuang15@outlook.com>
2023-10-11 13:48:46 +0800

    Add cmd to pull pcap out and ensure there is an adb server running.
    Signed-off-by: heinriciro <ianhuang15@outlook.com>

--- frida_get_keylog.bat
+++ frida_get_keylog.bat
@@ -1,7 +1,11 @@
 cd %~dp0
 
+::Ensure there is a adb server running
+adb start-server
+adb devices
+
 ::Start frida-server.
-adb shell "su -c setsid /data/local/tmp/frida-server-16.1.4-android-x86_64"
+adb shell "su -c 'setsid /data/local/tmp/frida-server-16.1.4-android-x86_64'"
 
 ::Instrument js code which will record constantly keylogs of SSL.
 start "ssh-key-logging" /i frida -U -l .\sslkeylogging.js -f cn.damai -o .\keylog.txt
@@ -15,13 +19,25 @@ start "tcp-dumping" /i "adb" shell "export PATH=/data/data/com.termux/files/usr/
 pause
 
 ::Kill tcpdump in Android
-%or pkill -f -9 tcpdump%
+::or pkill -f -9 tcpdump
 for /f "delims=" %%t in ('adb shell "ps -ef |grep tcpdump |grep -v grep |awk '{print $2}'"') do set TCPDUMP_PID=%%t
-@echo "Kill tcpdump - %TCPDUMP_PID%"
+@echo Kill tcpdump - %TCPDUMP_PID%
 adb shell "ps -ef|grep tcpdump| grep -v grep|awk '{print $2}'|su -c xargs kill -9"
 
-::Kill process
-call taskkill /f /im adb.exe
+::Kill frida-server in Android
+::or pkill -f -9 frida-server
+for /f "delims=" %%t in ('adb shell "ps -ef |grep frida-server |grep -v grep |awk '{print $2}'"') do set FRIDA_PID=%%t
+adb shell "su -c kill -9 %FRIDA_PID%"
+
+::or just kill -9 %TCPDUMP_PID%
+
+::Kill frida to stop get keylog
 call taskkill /f /im frida.exe
+
+::Pull the pcap out
+adb pull /data/local/tmp/tcp.pcap .\
+
+::Kill adb
+call taskkill /f /im adb.exe
 
 pause

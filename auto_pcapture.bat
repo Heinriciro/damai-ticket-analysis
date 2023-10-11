5ed875361e9c056e692940cbf32a65dc81884c20
heinriciro <ianhuang15@outlook.com>
2023-10-11 16:41:04 +0800

    Made more stable
    Signed-off-by: heinriciro <ianhuang15@outlook.com>

--- auto_pcapture.bat
+++ auto_pcapture.bat
@@ -5,7 +5,7 @@ adb start-server
 adb devices
 
 ::Start frida-server.
-adb shell "su -c 'setsid /data/local/tmp/frida-server-16.1.4-android-x86_64'"
+adb shell "cd /data/local/tmp & su -c setsid ./frida-server-16.1.4-android-x86_64"
 
 ::Instrument js code which will record constantly keylogs of SSL.
 start "ssh-key-logging" /i frida -U -l .\sslkeylogging.js -f cn.damai -o .\keylog.txt
@@ -27,6 +27,7 @@ adb shell "ps -ef|grep tcpdump| grep -v grep|awk '{print $2}'|su -c xargs kill -
 ::Kill frida-server in Android
 ::or pkill -f -9 frida-server
 for /f "delims=" %%t in ('adb shell "ps -ef |grep frida-server |grep -v grep |awk '{print $2}'"') do set FRIDA_PID=%%t
+@echo Kill frida-server - %FRIDA_PID%
 adb shell "su -c kill -9 %FRIDA_PID%"
 
 ::or just kill -9 %TCPDUMP_PID%
@@ -34,8 +35,9 @@ adb shell "su -c kill -9 %FRIDA_PID%"
 ::Kill frida to stop get keylog
 call taskkill /f /im frida.exe
 
-::Pull the pcap out
+::Pull the pcap out and remove
 adb pull /data/local/tmp/tcp.pcap .\
+adb shell "su -c rm -rf /data/local/tmp/tcp.pcap"
 
 ::Kill adb
 call taskkill /f /im adb.exe
